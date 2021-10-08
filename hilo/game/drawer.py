841184e7afb363d85df1f6431ed13c87657dad6c
Jadan White <34385383+Retrnon@users.noreply.github.com>
2021-10-08 17:55:56 -0600

    Update drawer.py
    added base methods to drawer.py

--- hilo/game/drawer.py
+++ hilo/game/drawer.py
@@ -5,4 +5,11 @@ class Drawer(object):
     def __init__(self):
         pass
 
-    
+    def draw_card(self):
+        pass
+
+    def score_card(self):
+        pass
+
+    def can_draw(self):
+        pass
