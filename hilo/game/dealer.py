29219bf7917f359a9a1a5b58d838da4449d2d58c
tr3vinj <jon20060@byui.edu>
2021-10-08 17:57:57 -0600

    Added dealer class

---
+++ hilo/game/dealer.py
@@ -0,0 +1,21 @@
+from drawer import Drawer
+
+class Dealer:
+    '''I will add documentation here later'''
+
+    def __init__(self):
+        self.keep_playing = True
+        self.score = 0
+        self.drawer = Drawer()
+
+    def start_game(self):
+        pass
+
+    def get_inputs(self):
+        pass
+
+    def do_updates(self):
+        pass
+
+    def do_outputs(self):
+        pass
