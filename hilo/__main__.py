3c608653dd95eb66f243ebd9dff7bd581a1821ea
Peter Benson <91146821+PBJ21033@users.noreply.github.com>
2021-10-08 18:45:04 -0600

    Peter and Jadan Commit

--- hilo/__main__.py
+++ hilo/__main__.py
@@ -1,1 +1,16 @@
-# TODO: Add entry point code here
+#Class 1: Director
+"""
+ask how much u ganna lose
+draw card
+ask to keep playing
+calls draw card function
+"""
+
+
+#Class 2: Thrower
+
+"""
+calc 100 if right 75 wrong
+if you get to 0 then you lose
+
+"""
--- hilo/game/drawer.py
+++ hilo/game/drawer.py
@@ -3,13 +3,35 @@ import random
 class Drawer(object):
 
     def __init__(self):
-        pass
+        self.new_card = 0
+        self.old_card = 0
+        self.turn_number = 0
 
     def draw_card(self):
-        pass
+        if self.turn_number == 0:
+            self.old_card = random.randint(1,13)
 
-    def score_card(self):
-        pass
+        self.new_card = random.randint(1,13)
+        return self.new_card, self.old_card
 
-    def can_draw(self):
-        pass
+    def score_card(self, higher):
+        self.turn_number += 1
+
+        if higher:
+            if self.new_card > self.old_card:
+                 return int(100)
+            else:
+                return int(-75) 
+
+        if not higher:
+            if self.new_card < self.old_card:
+                return int(100)
+            else:
+                return int(-75)
+            
+    def can_draw(self, score):
+        self.old_card = self.new_card
+        if bool(score) or self.turn_number == 0:
+            return True
+        else:
+            return False
